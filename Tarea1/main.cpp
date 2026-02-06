5ebe5a1343ba2c64c6c5ddb67d1fa0385f10e035
DelarosaJose <josevictordelarosachico694@gmail.com>
2026-02-06 14:27:00 -0600

    Tarea 1 / Completada

--- Tarea1/main.cpp
+++ Tarea1/main.cpp
@@ -13,6 +13,7 @@ int main() {
     Coches tuCarro(5, "BMW", "Rojo");
 
     //CARRO MIO (1 instancia)
+    cout << "INSTANCIA No 1 Nombre(miCarro)" << endl;
     cout << "Mi Carro Hace estas Acciones: " << miCarro.getMarca() << " Color " <<  miCarro.getColor()<<  endl;
     miCarro.arrancar();
     cout << "Una pista recta Dale aceleraaaaa !!! " << endl;
@@ -21,6 +22,7 @@ int main() {
     miCarro.frenar();
 
     //TU CARRO (2 instancia)
+    cout << "INSTANCIA No 2 Nombre(tuCarro)" << endl;
     cout << "Tu Carro Hace estas Acciones: " << tuCarro.getMarca() << " Color " << tuCarro.getColor() << endl;
     tuCarro.arrancar();
     cout <<  "Un pista Recta Dale Acelera wey!!" <<  endl;
